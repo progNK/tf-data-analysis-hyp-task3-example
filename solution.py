5b3d5255d3deb97d1aa4e8f8ffceff2d9609be34
Nadya <106408597+progNK@users.noreply.github.com>
2023-04-11 22:37:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import mannwhitneyu
+from scipy import stats
 
+chat_id = 1068869116 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.05
+    p = stats.mannwhitneyu(x, y).pvalue
+    return p < alpha
